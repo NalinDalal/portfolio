ce91f3e4b5dc968a48495641a2bff536c2b5a053
Nalin <nalindalal2004@gmail.com>
2025-08-11 18:48:32 +0530

    update the files to client side

--- components/PullRequestCard.tsx
+++ components/PullRequestCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { GitHubPullRequest } from '@/types/github';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
